e513129588640f9ba2e9615b9590328ec85fa725
Amarildo <amarildos@outloo.it>
2024-01-22 19:34:15 +0100

    added click function

--- js/script.js
+++ js/script.js
@@ -10,7 +10,28 @@ const app = createApp({
     data: () => ({
         pictures,
         currentIndex: 0
-    })
+    }),
+    computed: {
+        lastElementsIndex() {
+            return this.pictures.lenght - 1;
+        },
+        isFirstIndex() {
+            return this.currentIndex === 0;
+        },
+        isLastIndex() {
+            return this.currentIndex === this.lastElementsIndex;
+        }
+    },
+    methods: {
+        goToPrev(){
+            if(this.isFirstIndex) this.currentIndex = this.lastElementsIndex;
+            else this.currentIndex--;
+        },
+        goToNext() {
+            if(this.isLastIndex) this.currentIndex = 0;
+            else this.currentIndex++;
+        } 
+    }
 })
 
 app.mount('#root');
